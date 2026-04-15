0d600b25cfe60575b686ec2b07ee0fe7c4eff50c
daflh <dafiulhaq97@gmail.com>
2026-04-16 00:05:05 +0700

    emphasize shortky name

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,9 @@
     <!-- TopNavBar -->
     <header class="fixed top-0 w-full z-50 bg-[#081425]/60 backdrop-blur-3xl shadow-[0_40px_0_0_rgba(42,53,72,0.06)]">
       <div class="flex justify-between items-center px-6 py-4 max-w-7xl mx-auto">
-        <NuxtLink to="/" class="text-xl font-bold tracking-tighter text-[#8ed5ff] font-['Inter'] antialiased">
-          The Nocturnal Utility
+        <NuxtLink to="/" class="text-2xl font-bold tracking-tighter text-[#8ed5ff] font-['Inter'] antialiased">
+          Shortky
         </NuxtLink>
-        <nav class="hidden md:flex items-center gap-8">
-          <NuxtLink to="/" class="text-[#8ed5ff] font-semibold border-b-2 border-[#8ed5ff] pb-1 font-['Inter'] tracking-tight">Hub</NuxtLink>
-        </nav>
         <div class="flex items-center gap-4 text-[#8ed5ff]">
           <button class="hover:bg-[#152031] transition-all duration-300 p-2 rounded-xl active:scale-95">
             <span class="material-symbols-outlined">help_outline</span>
@@ -26,7 +23,7 @@
     <footer class="w-full py-8 mt-auto border-t border-[#3e484f]/15 bg-[#040e1f]">
       <div class="flex flex-col md:flex-row justify-between items-center px-8 gap-4 max-w-7xl mx-auto">
         <span class="text-xs font-medium uppercase tracking-widest font-['Inter'] text-[#bdc8d1]/60">
-          © 2024 The Nocturnal Utility. Organic Precision.
+          © 2024 Shortky.
         </span>
         <div class="flex gap-8">
           <a class="text-xs font-medium uppercase tracking-widest font-['Inter'] text-[#bdc8d1]/60 hover:text-[#8ed5ff] transition-colors cursor-pointer" href="#">Privacy</a>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap' }
       ],
-      title: 'Shortky | The Nocturnal Utility'
+      title: 'Shortky | Universal Link Sharing'
     }
   },
   devtools: { enabled: true },
